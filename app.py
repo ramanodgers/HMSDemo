5ccb077a4ac2e97f9df1f9bcb8232f859da1c536
ramanodgers <raman.odgers@hotmail.com>
2024-04-23 10:27:57 -0400

    HF space first launch"

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 import gradio as gr
 
 
-def greet(name):
-	return "Hello" + name
+description = ''' This is a Demo of our COMS 4995 Applied Computer Vision semester project: an ensemble method classification tool for EEG diagnoses.
+Drag and drop an image or eeg parquet file and (hopefully) you'll get your result 
 
-iface  = gr.Interface(fn=greet, inputs = 'text', outputs = 'text')
-iface.launch()
+Authors: Raman Odgers, Akhil Golla, Vinayak Kannan, Sohan Kshirsagar
+
+'''
+
+
+
+def greet(image, dist, choice):
+    
+
+    return ('gingus')
+
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=[gr.Image(), 
+            gr.Slider(0, 10, step = 1, label = "Error Distance"), 
+            gr.Radio(["Big CLDPC", "Small CLDPC","Surface 25","Steane 17"], label="Parity-Check matrix", info="What matrix should we use?", value = 'Big CLDPC')],
+
+    outputs=[gr.Textbox(label="EEG class")],
+
+    title="EEG Classification (APPLIED CV COMS 4995)",
+    description = description, 
+    live = True
+
+)
+demo.launch( share = False)
