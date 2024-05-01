accf0dd4d43fd687aea0cb0025b22185406a823a
ramanodgers <raman.odgers@hotmail.com>
2024-05-01 19:04:42 -0400

    other specfromeeg function

--- utils.py
+++ utils.py
@@ -17,6 +17,72 @@ FEATS = [['Fp1','F7','T3','T5','O1'],
          ['Fp2','F8','T4','T6','O2'],
          ['Fp2','F4','C4','P4','O2']]
 
+def spectrogram_from_eeg(parquet_path, display=False):
+    # LOAD MIDDLE 50 SECONDS OF EEG SERIES
+    eeg = pd.read_parquet(parquet_path)
+    middle = (len(eeg)-10_000)//2
+    eeg = eeg.iloc[middle:middle+10_000]
+
+    # VARIABLE TO HOLD SPECTROGRAM
+    img = np.zeros((128,256,4),dtype='float32')
+
+    if display:
+        plt.figure(figsize=(10,7))
+    signals = []
+    for k in range(4):
+        COLS = FEATS[k]
+
+        for kk in range(4):
+
+            # COMPUTE PAIR DIFFERENCES
+            x = eeg[COLS[kk]].values - eeg[COLS[kk+1]].values
+
+            # FILL NANS
+            m = np.nanmean(x)
+            if np.isnan(x).mean()<1: x = np.nan_to_num(x,nan=m)
+            else: x[:] = 0
+
+            # # DENOISE
+            # if USE_WAVELET:
+            #     x = denoise(x, wavelet=USE_WAVELET)
+            signals.append(x)
+
+            # RAW SPECTROGRAM
+            mel_spec = librosa.feature.melspectrogram(y=x, sr=200, hop_length=len(x)//256,
+                  n_fft=1024, n_mels=128, fmin=0, fmax=20, win_length=128)
+
+            # LOG TRANSFORM
+            width = (mel_spec.shape[1]//32)*32
+            mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max).astype(np.float32)[:,:width]
+
+            # STANDARDIZE TO -1 TO 1
+            mel_spec_db = (mel_spec_db+40)/40
+            img[:,:,k] += mel_spec_db
+
+        # AVERAGE THE 4 MONTAGE DIFFERENCES
+        img[:,:,k] /= 4.0
+
+        if display:
+            plt.subplot(2,2,k+1)
+            plt.imshow(img[:,:,k],aspect='auto',origin='lower')
+            # plt.title(f'EEG {eeg_id} - Spectrogram {NAMES[k]}')
+
+    if display:
+        plt.show()
+        plt.figure(figsize=(10,5))
+        offset = 0
+        for k in range(4):
+            if k>0: offset -= signals[3-k].min()
+            plt.plot(range(10_000),signals[k]+offset,label=NAMES[3-k])
+            offset += signals[3-k].max()
+        plt.legend()
+        # plt.title(f'EEG {eeg_id} Signals')
+        plt.show()
+        print(); print('#'*25); print()
+
+    return img
+
+
 def features_from_eeg(eegs, display=False):
     for eeg in eegs:
         ids_data = np.array([], dtype=np.int64)
